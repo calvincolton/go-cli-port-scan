f6a35549940b10bb786a5131eb12c3f48cacab14
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2023-12-10 14:03:52 -0800

    feat: scan actions and testing

--- cmd/actions_test.go
+++ cmd/actions_test.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -94,6 +96,60 @@ func TestHostActions(t *testing.T) {
 	}
 }
 
+func TestScanActions(t *testing.T) {
+	hosts := []string{
+		"localhost",
+		"unknownHostOutThere",
+	}
+
+	tf, cleanup := setup(t, hosts, true)
+	defer cleanup()
+
+	ports := []int{}
+
+	// Init ports, 1 open, 1 closed
+	for i := 0; i < 2; i++ {
+		ln, err := net.Listen("tcp", net.JoinHostPort("localhost", "0"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer ln.Close()
+
+		_, portStr, err := net.SplitHostPort(ln.Addr().String())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		ports = append(ports, port)
+
+		if i == 1 {
+			ln.Close()
+		}
+	}
+
+	expectedOut := fmt.Sprintln("localhost:")
+	expectedOut += fmt.Sprintf("\t%d: open\n", ports[0])
+	expectedOut += fmt.Sprintf("\t%d: closed\n", ports[1])
+	expectedOut += fmt.Sprintln()
+	expectedOut += fmt.Sprintln("unknownHostOutThere: Host not found")
+	expectedOut += fmt.Sprintln()
+
+	var out bytes.Buffer
+	// Execute scan and capture output
+	if err := scanAction(&out, tf, ports); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+	// Test scan output
+	if out.String() != expectedOut {
+		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
+	}
+}
+
 func TestIntegration(t *testing.T) {
 	hosts := []string{
 		"host1",
@@ -122,22 +178,31 @@ func TestIntegration(t *testing.T) {
 	expectedOut += fmt.Sprintf("Deleted host: %s\n", delHost)
 	expectedOut += strings.Join(hostsEnd, "\n")
 	expectedOut += fmt.Sprintln()
+	for _, v := range hostsEnd {
+		expectedOut += fmt.Sprintf("%s: Host not found\n", v)
+		expectedOut += fmt.Sprintln()
+	}
 
-	// Execute operations for add -> list -> delete -> list, as a user would
-	// add hosts to the list
+	// Execute operations for add -> list -> delete -> list -> scan, as a user would
+	// Add hosts to the list
 	if err := addAction(&out, tf, hosts); err != nil {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
-	// list hosts
+	// List hosts
 	if err := listAction(&out, tf, nil); err != nil {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
-	// delete host2
+	// Delete host2
 	if err := deleteAction(&out, tf, []string{delHost}); err != nil {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
-	// list hosts after delete
+	// List hosts after delete
 	if err := listAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	// Scan hosts
+	if err := scanAction(&out, tf, nil); err != nil {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
 
